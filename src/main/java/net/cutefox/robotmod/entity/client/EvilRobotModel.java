1e0525cdb17fe595794b3ba4fa17d3caad2436d7
CuteFox8302 <111240985+CuteFox8302@users.noreply.github.com>
2022-12-31 17:29:43 -0500

    Added Stuff.

--- src/main/java/net/cutefox/robotmod/entity/client/EvilRobotModel.java
+++ src/main/java/net/cutefox/robotmod/entity/client/EvilRobotModel.java
@@ -1,5 +1,6 @@
 package net.cutefox.robotmod.entity.client;
 
+import net.cutefox.robotmod.RobotMod;
 import net.cutefox.robotmod.entity.custom.EvilRobotEntity;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
@@ -7,16 +8,16 @@ import software.bernie.geckolib3.model.AnimatedGeoModel;
 public class EvilRobotModel extends AnimatedGeoModel<EvilRobotEntity> {
     @Override
     public Identifier getModelResource(EvilRobotEntity object) {
-        return null;
+        return new Identifier(RobotMod.MOD_ID, "geo/evil_robot.geo.json");
     }
 
     @Override
     public Identifier getTextureResource(EvilRobotEntity object) {
-        return null;
+        return new Identifier(RobotMod.MOD_ID, "textures/entity/evil_robot_texture.png");
     }
 
     @Override
     public Identifier getAnimationResource(EvilRobotEntity animatable) {
-        return null;
+        return new Identifier(RobotMod.MOD_ID, "animations/evil_robot.animation.json");
     }
 }
