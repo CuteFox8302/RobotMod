db243146ab2d6681cb597e9e65dae87904aeea57
CuteFox8302 <111240985+CuteFox8302@users.noreply.github.com>
2022-12-31 16:32:32 -0500

    Added Stuff.

---
+++ src/main/java/net/cutefox/robotmod/item/ModItemGroup.java
@@ -0,0 +1,13 @@
+package net.cutefox.robotmod.item;
+
+import net.cutefox.robotmod.RobotMod;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroup {
+    public static final ItemGroup ROBOTS = FabricItemGroupBuilder.build(
+            new Identifier(RobotMod.MOD_ID, "robots"), () -> new ItemStack(Items.EVOKER_SPAWN_EGG));
+}
